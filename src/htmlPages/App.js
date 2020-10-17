314eb726092ede68cb0d006d2746cfed36de3fcc
Meenakshi Sharma Bhardwaj <Meenakshisharmab651@gmail.com>
2020-10-17 20:49:31 +0530

    Changes

--- src/htmlPages/App.js
+++ src/htmlPages/App.js
@@ -3,12 +3,14 @@ import '../css/styles.css';
 import Button from '../htmlPages/Button';
 function App(){
     return (
+        
         <div>
         <h1 style={{textAlign:"center"}}>Hello World!!!</h1>
         <Button title="Play Store"></Button>
         <Button title="Apple Store"></Button>
         </div>
         );
+
 }
 
 export default App;
