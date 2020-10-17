51efd164fad7a4de6714659543c6e3fe3878fb14
Meenakshi Sharma Bhardwaj <Meenakshisharmab651@gmail.com>
2020-10-17 20:24:01 +0530

    Changes

---
+++ src/htmlPages/App.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+import '../css/styles.css';
+import Button from '../htmlPages/Button';
+function App(){
+    return (
+        <div>
+        <h1 style={{textAlign:"center"}}>Hello World!!!</h1>
+        <Button title="Play Store"></Button>
+        <Button title="Apple Store"></Button>
+        </div>
+        );
+}
+
+export default App;
---
+++ src/htmlPages/Button.js
@@ -0,0 +1,6 @@
+import '../css/styles.css';
+import React from 'react';
+const Button=({title})=>{
+return(<span className="button">{title}</span>);
+}
+export default  Button;
